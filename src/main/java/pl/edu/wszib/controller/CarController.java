1bbf71ce1281aae4a24648bc9af5aa3ba3162f15
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-08-22 17:32:21 +0200

    small changes in CarController

--- src/main/java/pl/edu/wszib/controller/CarController.java
+++ src/main/java/pl/edu/wszib/controller/CarController.java
@@ -1,35 +1,43 @@
 package pl.edu.wszib.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pl.edu.wszib.domain.entity.Car;
 import pl.edu.wszib.domain.entity.CarType;
+import pl.edu.wszib.domain.repository.CarRepository;
+import pl.edu.wszib.domain.repository.CarTypeRepository;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
 
-    //TODO: write implementation to all those methods
+    @Autowired
+    CarRepository carRepository;
 
-    @RequestMapping(value = "/{id}")
-    @GetMapping
-    public String getCarById(@PathVariable("id") Integer id) {
-        return "car by id";
+    @Autowired
+    CarTypeRepository carTypeRepository;
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Integer id) {
+        return carRepository.getOne(id);
     }
 
-    @RequestMapping(value = "/{brand}")
-    @GetMapping
-    public String getCarByBrand(@PathVariable("brand") String brand) {
+    //TODO: write implementation to all those methods
+
+    @GetMapping("/{type}")
+    public String getCarByType(@PathVariable String type) {
         return "car by brand";
     }
 
     @RequestMapping(value = "/brand/{model}")
     @GetMapping
-    public String getCarByModel(@PathVariable("model") String model) {
+    public String getCarByModel(@PathVariable String model) {
         return "car by model";
     }
 
     @RequestMapping(value = "/{enginecapacity}")
     @GetMapping
-    public String getCarByEngineCapacity(@PathVariable("enginecapacity") String enginecapacity) {
+    public String getCarByEngineCapacity(@PathVariable String enginecapacity) {
         return "car by engine capacity";
     }
 
@@ -45,16 +53,16 @@ public class CarController {
         return "added car";
     }
 
-    @RequestMapping(value = "/{id}")
+    /*@RequestMapping(value = "/{id}")
     @PostMapping
     public String updateCarById(@PathVariable("id") Integer id) {
         return "car updated";
-    }
+    }*/
 
-    @RequestMapping(value = "/{id}")
+    /*@RequestMapping(value = "/{id}")
     @DeleteMapping
     public String delCarById(@PathVariable("id") Integer id) {
         return "car removed";
-    }
+    }*/
 
 }
