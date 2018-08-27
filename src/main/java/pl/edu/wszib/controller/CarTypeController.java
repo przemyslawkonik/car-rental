4d2310a075a8dd504eb608a061af19625be2b711
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-08-27 22:35:55 +0200

    added CarTypeController and new methods into it

--- src/main/java/pl/edu/wszib/controller/CarTypeController.java
+++ src/main/java/pl/edu/wszib/controller/CarTypeController.java
@@ -1,5 +1,7 @@
 package pl.edu.wszib.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,30 +9,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import pl.edu.wszib.domain.entity.CarType;
 import pl.edu.wszib.domain.repository.CarTypeRepository;
 
+import java.util.List;
+
 public class CarTypeController {
+    private static final Logger logger = LogManager.getLogger(CarTypeController.class);
+
+    // Todo: we should possibly add rest of the CRUD methods
+
     @Autowired
     CarTypeRepository carTypeRepository;
 
+    @GetMapping("/")
+    public List<CarType> getAllCars() {
+        logger.info("Retrieving all types of cars from database");
+        return carTypeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CarType getCarById(@PathVariable Integer id) {
+        logger.info("Retrieving car type by id: " + id);
+        return carTypeRepository.findById(id);
+    }
+
+    @GetMapping("/{brand}")
+    public CarType getCarByModel(@PathVariable String brand) {
+        logger.info("Retrieving car type by model: " + brand);
+        return carTypeRepository.findByBrand(brand);
+    }
+
     @GetMapping("/{brand}/{model}")
     public CarType getCarByModel(@PathVariable String brand, @PathVariable String model) {
+        logger.info("Retrieving car type by model: " + model);
         return carTypeRepository.findByModel(brand, model);
     }
 
     @GetMapping("/{enginecapacity}")
     public CarType getCarByEngineCapacity(@PathVariable String enginecapacity) {
+        logger.info("Retrieving car type by engine capacity: " + enginecapacity);
         return carTypeRepository.findByEngineCapacity(enginecapacity);
     }
 
 
     @GetMapping("/{enginetype}")
     public CarType getCarByEngineType(@PathVariable String enginetype) {
+        logger.info("Retrieving car type by engine type: " + enginetype);
         return carTypeRepository.findByEngineType(enginetype);
-    }
-
-    @PostMapping("/{cartype}")
-    public void addTypeOfCar(@PathVariable CarType cartype) {
-        carTypeRepository.save(cartype);
-
-        System.out.println("Car type saved!");
     }
 }
