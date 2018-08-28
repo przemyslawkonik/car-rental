13c3e63fce485534d24ba5b55babb800d85f6618
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-08-28 20:06:56 +0200

    added new methods to AddressController

--- src/main/java/pl/edu/wszib/controller/AddressController.java
+++ src/main/java/pl/edu/wszib/controller/AddressController.java
@@ -3,14 +3,46 @@ package pl.edu.wszib.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.wszib.domain.entity.Address;
 import pl.edu.wszib.domain.repository.AddressRepository;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class AddressController {
     private static final Logger logger = LogManager.getLogger(CarController.class);
 
+    // todo: re-think how we should organize that
     @Autowired
     private AddressRepository addressRepository;
 
+    @GetMapping("/{customerId}/address")
+    public Address getAddressByCustomerId(@PathVariable Integer customerId) {
+        logger.info("Retrieving address by customer id: " + customerId);
+        return addressRepository.findByCustomerId(customerId);
+    }
+
+    @GetMapping("/addresses")
+    public List<Address> getAllAddresses() {
+        logger.info("Retrieving all addresses");
+        return addressRepository.findAll();
+    }
+
+    //todo: how to connect given address with particular customer ?!
+    @PostMapping("/{customerId}/address")
+    public Address addAddress (@PathVariable Integer customerId, @RequestBody Address address) {
+        logger.info("Adding new address");
+        return addressRepository.save(address);
+    }
+
+    //todo: how to update this address ?!
+    @PutMapping("/{customerId}/address")
+    public Address updateAddress (@PathVariable Integer customerId, @RequestBody Address address) {
+        logger.info("Update new address");
+        return addressRepository.save(address);
+    }
 
 
 
