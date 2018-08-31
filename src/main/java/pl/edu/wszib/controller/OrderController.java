86aa14fb9746506a02edfbef3fb50175e0ac628c
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-08-31 20:56:46 +0200

    OrderController added

---
+++ src/main/java/pl/edu/wszib/controller/OrderController.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+}
+
