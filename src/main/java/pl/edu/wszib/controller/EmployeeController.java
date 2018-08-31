870b363324a35048310b79c7f038f55ed94bdeaf
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-08-31 18:35:08 +0200

    added PUT and POST methods

--- src/main/java/pl/edu/wszib/controller/EmployeeController.java
+++ src/main/java/pl/edu/wszib/controller/EmployeeController.java
@@ -1,10 +1,9 @@
 package pl.edu.wszib.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.wszib.domain.entity.Employee;
 import pl.edu.wszib.domain.repository.EmployeeRepository;
 
@@ -17,28 +16,50 @@ public class EmployeeController {
     // I'm not adding fields which are using field idNumber from Employee class
     // - because I'm not sure do we really need it
 
+    private static final Logger logger = LogManager.getLogger(EmployeeController.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @GetMapping("/")
     public List<Employee> getAllEmployees() {
+        logger.info("Retrieving all employees from database");
         return employeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Integer id) {
+        logger.info("Retrieving employee with id: " + id);
         return employeeRepository.findById(id);
     }
 
     @GetMapping("/{name}")
     public Employee getEmployeeByName(@PathVariable String name) {
+        logger.info("Retrieving employee with name: " + name);
         return employeeRepository.findByName(name);
     }
 
     @GetMapping("/{surname}")
     public Employee getEmployeeBySurname(@PathVariable String surname) {
+        logger.info("Retrieving employee with surname: " + surname);
         return employeeRepository.findBySurname(surname);
     }
 
+    @PostMapping("/")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        logger.info("Adding employee");
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateCar(@PathVariable Integer id, @RequestBody Employee employeeDetails) {
+        logger.info("Updating employee with id: " + id);
+
+        Employee employee = employeeRepository.findById(id);
+        employee.copyFieldsState(employeeDetails);
+        Employee updatedEmployee = employeeRepository.save(employee);
+
+        return updatedEmployee;
+    }
 
 }
