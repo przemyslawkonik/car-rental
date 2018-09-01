a433c5cecbb742e3c352308a88a15339322b5784
Filip Mastalerz <filip.mastalerz@gmail.com>
2018-09-01 11:46:50 +0200

    safe commit

--- src/main/java/pl/edu/wszib/domain/repository/CarRepository.java
+++ src/main/java/pl/edu/wszib/domain/repository/CarRepository.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Repository;
 import pl.edu.wszib.domain.entity.Car;
 import pl.edu.wszib.domain.entity.CarType;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
     Car findById(Integer id);
 
-    Car findByCarType(String cartype);
+    List<Car> findByCarType(String cartype);
 
     Car findByRentPrice(String rentprice);
 
