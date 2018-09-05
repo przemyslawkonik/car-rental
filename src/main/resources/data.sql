da3998d1c7c2047a2efa33efe86789aaad698313
przemyslaw <przemyslawkonik@gmail.com>
2018-09-05 19:30:23 +0200

    db script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO car_type (id, brand, model, engineCapacity, engineType) VALUES (1, 'brandTest1', 'modelTest1', 'engineCapacityTest1', 'engineTypeTest1');
+INSERT INTO car_type (id, brand, model, engineCapacity, engineType) VALUES (2, 'brandTest2', 'modelTest2', 'engineCapacityTest2', 'engineTypeTest2');
+INSERT INTO car_type (id, brand, model, engineCapacity, engineType) VALUES (3, 'brandTest3', 'modelTest3', 'engineCapacityTest3', 'engineTypeTest3');
+INSERT INTO car_type (id, brand, model, engineCapacity, engineType) VALUES (4, 'brandTest4', 'modelTest4', 'engineCapacityTest4', 'engineTypeTest4');
+INSERT INTO car_type (id, brand, model, engineCapacity, engineType) VALUES (5, 'brandTest5', 'modelTest5', 'engineCapacityTest5', 'engineTypeTest5');
+
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (1, 'registrationNumberTest1', 'colorTest1', 'productionYearTest1', 'mileageTest1', 'rentPriceTest1', 2);
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (2, 'registrationNumberTest2', 'colorTest2', 'productionYearTest2', 'mileageTest2', 'rentPriceTest2', 2);
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (3, 'registrationNumberTest3', 'colorTest3', 'productionYearTest3', 'mileageTest3', 'rentPriceTest3', 3);
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (4, 'registrationNumberTest4', 'colorTest4', 'productionYearTest4', 'mileageTest4', 'rentPriceTest4', 1);
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (5, 'registrationNumberTest5', 'colorTest5', 'productionYearTest5', 'mileageTest5', 'rentPriceTest5', 5);
+INSERT INTO car (id, registrationNumber, color, productionYear, mileage, rentPrice, carType_id) VALUES (6, 'registrationNumberTest6', 'colorTest6', 'productionYearTest6', 'mileageTest6', 'rentPriceTest6', 4);
+
